a66b6e3da3e982887bb3a09277adff18cf1cbbcb
NouriHicham <156692558+NouriHicham@users.noreply.github.com>
2025-05-07 15:57:53 +0000

    users

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        //validate the request with library
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:5|confirmed',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        //create user
+        $user = \App\Models\User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        //return user
+        return response()->json([
+            'user' => $user,
+            'message' => 'User created successfully',
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        //validate the request with library
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:8',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        //check if user exists
+        $user = \App\Models\User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+        //create token
+        $token = JWTAuth::attempt($request->only('email', 'password'));
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+        ]);
+        //validate the credentials with library
+        try {
+            if(!$token=JWTAuth::attempt($credentials)){
+                return response()->json(['error' => 'invalid_credentials'], 401);
+
+            }
+            return response()->json([
+                'message' => 'User logged in successfully',
+                'token' => $token,
+            ], 200);
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+
+    }
+
+    public function getUser()
+    {
+        $user = Auth::user();
+        return response()->json([
+            'message' => 'User retrieved successfully',
+            'data' => $user,
+        ], 200);
+    }
+
+    public function logout()
+    {
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+            return response()->json([
+                'message' => 'User logged out successfully',
+            ], 200);
+        } catch (JWTException $e) {
+            return response()->json([
+                'message' => 'Failed to logout, please try again',
+            ], 500);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\PokemonController;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsUserAuth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,20 +11,29 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// devuelve la lista de pokemon
+// rutas publicas
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 Route::get('/pokemon', [PokemonController::class, 'index']);
-
-// devuelve un pokemon
 Route::get('/pokemon/{id}', [PokemonController::class, 'show']);
 
-// crear un pokemon
-Route::post('/pokemon', [PokemonController::class, 'store']);
+// routes protegidas
+Route::middleware([IsUserAuth::class])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('user', [AuthController::class, 'getUser']);
+    Route::post('/pokemon', [PokemonController::class, 'store']);
+});
 
-// actualizar todos los campos de un pokemon
-Route::put('/pokemon/{id}', [PokemonController::class, 'update']);
+//rutas admin
+Route::middleware([IsAdmin::class])->group(function () {
+    Route::get('users', [AuthController::class, 'getUsers']);
+    Route::get('user/{id}', [AuthController::class, 'getUser']);
+    Route::put('user/{id}', [AuthController::class, 'updateUser']);
+    Route::delete('user/{id}', [AuthController::class, 'deleteUser']);
+    //pokemon
+    Route::post('/pokemon', [PokemonController::class, 'store']);
+    Route::put('/pokemon/{id}', [PokemonController::class, 'update']);
+    Route::patch('/pokemon/{id}', [PokemonController::class, 'updatePartial']);
+    Route::delete('/pokemon/{id}', [PokemonController::class, 'destroy']);
 
-// actualizar el parcialmente un pokemon
-Route::patch('/pokemon/{id}', [PokemonController::class, 'updatePartial']);
-
-// eliminar un pokemon
-Route::delete('/pokemon/{id}', [PokemonController::class, 'destroy']);
+});
